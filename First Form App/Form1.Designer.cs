fd904bc25b8f5c75e28a6d9b7d49c55680e5b303
Rishav <rishav394@gmail.com>
2018-04-14 01:45:18 +0530

    Updates Icon

--- First Form App/Form1.Designer.cs
+++ First Form App/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(246, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 60);
+            this.label1.Multiline = true;
+            this.label1.Name = "label1";
+            this.label1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.label1.Size = new System.Drawing.Size(369, 448);
+            this.label1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Berlin Sans FB", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(39, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(369, 508);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Skillack Code Finder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- First Form App/Form1.cs
+++ First Form App/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace First_Form_App
@@ -15,6 +8,103 @@ namespace First_Form_App
         public Form1()
         {
             InitializeComponent();
+            button1.Enabled = false;
+            textBox1.Select();
+            label1.ReadOnly = true;
+            label1.Visible = false;
         }
+        
+        WebBrowser wb = new WebBrowser();
+        WebBrowser wb2 = new WebBrowser();
+        bool ct = true;
+        string pagelink = "";
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            ct = true;
+            label1.Text = "";
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+            wb.ScriptErrorsSuppressed = true;
+            string temp= "https://vitspot.com/?s="+textBox1.Text;
+            wb.Navigate(temp);
+            wb.DocumentCompleted += Wb_DocumentCompleted;
+        }
+
+        private void Wb_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (ct)
+            {
+                var links = wb.Document.GetElementsByTagName("a");
+                foreach (HtmlElement link in links)
+                {
+                    if (link.GetAttribute("className") == "btn dark medium")
+                    {
+                        pagelink = link.GetAttribute("href");
+                        break;
+                    }
+                }
+                
+                
+                wb2.ScriptErrorsSuppressed = true;
+                wb2.Navigate(pagelink.Trim());
+               
+                wb2.DocumentCompleted += Wb2_DocumentCompleted;
+            }
+        }
+
+        private void Wb2_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (ct)
+            {
+                ct = false;
+                button1.Enabled = true;
+                textBox1.Enabled = true;
+                
+                //label1.Font=new Font("Times New Roman", 12);
+                if (Findid() == null)
+                {
+                    MessageBox.Show("Sorry the entered code was not found.","Please try another id");
+                    return;
+                }
+                string[] words = pagelink.Split('/');
+                words[4] = words[4].Replace('-', ' ').ToUpper().Trim();
+               
+                MessageBox.Show( words[4] + Environment.NewLine+ "Opening now!","Found");
+                string data= wb2.Document.GetElementById(Findid()).InnerText;
+                label1.Visible = true;
+                label1.Text = data;
+                
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                button1.Enabled = false;
+            }
+        }
+
+        public string Findid()
+        {
+            var links = wb2.Document.GetElementsByTagName("div");
+
+            foreach (HtmlElement link in links)
+            {
+                 if (link.GetAttribute("className") == "crayon-syntax crayon-theme-sublime-text crayon-font-monaco crayon-os-pc print-yes notranslate")
+                {
+                    return link.GetAttribute("id");
+                }
+            }
+            return null;
+        }
+
+      
+
     }
 }
